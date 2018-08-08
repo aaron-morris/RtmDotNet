3345fa6e20ae4f616c189ef5e40c9096e7f80dc5
Aaron Morris <morris7200@gmail.com>
2018-08-08 05:34:38 -0400

    Initialize a new Avalonia UI client project using the default MVVC template project.

---
+++ RtmDotNet.ClientApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RtmDotNet.ClientApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
---
+++ RtmDotNet.ClientApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace RtmDotNet.ClientApp.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
