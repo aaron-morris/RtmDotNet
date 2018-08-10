c122a2caa209db751169c473ff8b8ffcf2de1f0d
Aaron Morris <morris7200@gmail.com>
2018-08-09 20:14:44 -0400

    Implement basic RTM "Hello World" by displaying a list of projects in the window.

--- RtmDotNet.ClientApp/ViewModels/MainWindowViewModel.cs
+++ RtmDotNet.ClientApp/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ReactiveUI;
+using RtmDotNet.ClientApp.Models;
+using RtmDotNet.Lists;
+using RtmDotNet.Users;
 
 namespace RtmDotNet.ClientApp.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ReactiveObject
     {
-        public string Greeting => "Hello World!";
+        private IRtmUser _user;
+
+        private IList<RtmList> _lists;
+
+        public MainWindowViewModel()
+        {
+            _lists = new List<RtmList>();
+            Initialize();
+        }
+
+        private async Task Initialize()
+        {
+            var rtmConfigs = JsonConvert.DeserializeObject<RtmConfigs>(File.ReadAllText("rtmConfigs.json"));
+            Rtm.Init(rtmConfigs.ApiKey, rtmConfigs.SharedSecret);
+            _user = Rtm.GetUserFactory().LoadFromJson(File.ReadAllText("myRtmUser.json"));
+            var lists = await Rtm.GetListRepository(_user.Token).GetAllListsAsync();
+            Children = lists.ToList().OrderBy(x => x.Position).ThenBy(x => x.Name).ToList();
+        }
+
+        public IList<RtmList> Children
+        {
+            get => _lists;
+            set => this.RaiseAndSetIfChanged(ref _lists, value);
+        }
     }
 }
