f8ad9aa6d3697d82119e71ea083b8c2b5fdb3001
Aaron Morris <morris7200@gmail.com>
2018-08-10 04:21:28 -0400

    Replace Integration Tests with Examples

---
+++ RtmDotNet.Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RtmDotNet.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
